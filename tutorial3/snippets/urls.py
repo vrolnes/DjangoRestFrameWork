67aa3116a6bd0d09da2f58adbb5d8079e178bae9
enes34bjk <35971211+enes34bjk@users.noreply.github.com>
2020-04-06 14:37:26 +0300

    File Upload

---
+++ tutorial3/snippets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
