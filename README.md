f6709e9a771d5438b1a93cd09d4655f7e3ba1ea2
enes34bjk <35971211+enes34bjk@users.noreply.github.com>
2020-04-06 18:36:21 +0300

    File Upload

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # DjangoRestFrameWork
 Django Rest Frame Work Practices
-This branch is created by following page: https://www.django-rest-framework.org/tutorial/3-class-based-views/
+This branch is created by following page: https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/
---
+++ tutorial5/snippets/views.py
@@ -0,0 +1,27 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer,UserSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+
+class SnippetList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
